282b5264152dbc2631d032c38fa6ed97326a14f9
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2026-04-28 17:15:38 +0530

    Refactor MembershipDAO to support CRUD operations and improve extensibility

--- src/main/java/com/luv2code/aopdemo/dao/MembershipDAO.java
+++ src/main/java/com/luv2code/aopdemo/dao/MembershipDAO.java
@@ -2,6 +2,16 @@ package com.luv2code.aopdemo.dao;
 
 public interface MembershipDAO {
 
+    // Core operations
+    boolean addMember(String memberName, String membershipType);
+
+    boolean updateMember(String memberName, String membershipType);
+
+    boolean deleteMember(String memberName);
+
+    String findMember(String memberName);
+
+    // Demo / utility methods (used for AOP examples)
     boolean addSillyMember();
 
     void goToSleep();
